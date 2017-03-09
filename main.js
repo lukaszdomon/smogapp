b195276292e273a83bb2b6963e48de49c4c7266b
lukaszdomon <lukasz.domon@gmail.com>
2017-03-09 20:51:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 $(function () {
-  $('[data-toggle="popover"]').popover()
-})
+      $('#station1').popover({
+        html: true,
+        placement: "right",
+        trigger: "hover",
+        title: function () {
+            return $("#station1 .pop-title").html();
+        },
+        content: function () {
+            return $("#station1 .pop-content").html();
+        }
+      });
+    });
